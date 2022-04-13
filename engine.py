bfe4dca486b9898c32264b4e35149ae723cedee8
Anil Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2022-04-13 15:30:16 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -32,6 +32,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
 
         outputs = model(samples)
         loss_dict = criterion(outputs, targets)
+        print('loss_dict:', loss_dict)
         weight_dict = criterion.weight_dict
         losses = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
 
