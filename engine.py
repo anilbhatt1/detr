b20c3b410906e482fc28ab57b9f51d04a661903f
Anil Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2022-04-18 13:24:45 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -29,7 +29,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
     iter__ = 0
     for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
         iter__ += 1
-        print(f'Engine - iter : {iter__}, samples.size() : {samples.tensors.shape}, targets.size() : {type(targets[0])}, {type(targets[1]}')
+        print(f'Engine - iter : {iter__}, samples.size() : {samples.tensors.shape}, targets.size() : {type(targets[0])}, {type(targets[1])}')
         samples = samples.to(device)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
