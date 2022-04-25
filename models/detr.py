4ee6b3fde99d5b30aee129145489b4d82bbad03c
Anil Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2022-04-25 14:41:04 +0530

    Update detr.py

--- models/detr.py
+++ models/detr.py
@@ -163,7 +163,7 @@ class SetCriterion(nn.Module):
 
         loss_giou = 1 - torch.diag(box_ops.generalized_box_iou(
             box_ops.box_cxcywh_to_xyxy(src_boxes),
-            box_ops.box_cxcywh_to_xyxy(target_boxes)))
+            box_ops.box_cxcywh_to_xyxy(target_boxes), print_flag))
         losses['loss_giou'] = loss_giou.sum() / num_boxes
         return losses
 
